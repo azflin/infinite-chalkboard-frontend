55b324a937cfaa249becb7a25a9d4bc9db7c35e9
Allen Lin <allenlin1992@hotmail.com>
2021-08-15 19:23:36 -0400

    continue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import { Container, Col, Row, Button } from 'react-bootstrap';
 import './App.css';
+import detectEthereumProvider from '@metamask/detect-provider';
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
 function App() {
+
+  const [provider, setProvider] = useState();
+  const [signer, setSigner] = useState();
+  const [address, setAddress] = useState();
+
+  const connectMetamask = async () => {
+    if (provider) {
+      try {
+        let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAddress(accounts[0]);
+        setSigner(provider.getSigner());
+      } catch {
+        console.log("User rejected connection request.")
+      }
+    }
+  }
+
+  const logStuff = () => {
+    console.log(signer);
+  }
+
+  useEffect(() => {
+    async function getProvider() {
+      let p = new ethers.providers.Web3Provider(window.ethereum);
+      let accounts = await p.listAccounts();
+      if (accounts.length) {
+        setAddress(accounts[0]);
+        setSigner(p.getSigner());
+      }
+      setProvider(p);
+    }
+    getProvider();
+  }, []);
+
   return (
     <Container className="mt-2">
       <div align="right">
-        <Button>Connect</Button>
+        {address ? address : <Button onClick={connectMetamask}>Connect</Button>}
       </div>
+      <Button onClick={logStuff}>Log Stuff</Button>
     </Container>
   );
 }
