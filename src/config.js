17c7f72ac61ad4d25ad6cf67e1d6767910225b3a
Allen Lin <azflin@users.noreply.github.com>
2021-08-18 18:58:18 -0400

    rinkeby

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
-// export const INFINITE_CHALKBOARD_ADDRESS = "0x3a3838cE89B39B05FDFb9D307778C6EA7340e42C";
-// export const NETWORK = {
-//   chainId: 4,
-//   block_explorer_url: "https://rinkeby.etherscan.io/",
-//   name: "rinkeby"
-// }
-
-export const INFINITE_CHALKBOARD_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+export const INFINITE_CHALKBOARD_ADDRESS = "0x3a3838cE89B39B05FDFb9D307778C6EA7340e42C";
 export const NETWORK = {
-  chainId: 31337,
+  chainId: 4,
   block_explorer_url: "https://rinkeby.etherscan.io/",
-  name: "localhost"
-};
+  name: "rinkeby"
+}
+
+// export const INFINITE_CHALKBOARD_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+// export const NETWORK = {
+//   chainId: 31337,
+//   block_explorer_url: "https://rinkeby.etherscan.io/",
+//   name: "localhost"
+// };
 
 export const CURRENCY = "ETH";
