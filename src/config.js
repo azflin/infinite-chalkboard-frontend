93a06b15a6e2747d740d1331ffe4a1b4ddb3a3ec
Allen Lin <azflin@users.noreply.github.com>
2021-08-18 19:08:17 -0400

    smol change

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ export const INFINITE_CHALKBOARD_ADDRESS = "0x3a3838cE89B39B05FDFb9D307778C6EA73
 export const NETWORK = {
   chainId: 4,
   block_explorer_url: "https://rinkeby.etherscan.io/",
-  name: "rinkeby"
+  name: "rinkeby",
+  currency: "ETH"
 }
 
 // export const INFINITE_CHALKBOARD_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
@@ -11,5 +12,3 @@ export const NETWORK = {
 //   block_explorer_url: "https://rinkeby.etherscan.io/",
 //   name: "localhost"
 // };
-
-export const CURRENCY = "ETH";
